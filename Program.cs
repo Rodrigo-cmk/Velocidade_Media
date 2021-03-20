aae41ceafc9fc75280436501ca981509908976d2
Rodrigo Alves <rodrigorac.2003@gmail.com>
2021-03-20 11:28:28 -0300

    Fnalizando Cálculo da VelMedia

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Velocidade_Media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            double tempo;
+            double distancia;
+            double velocidade;
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("---- Exercício Velocidade Média ----");
+            Console.WriteLine("Cálculo da velocidade, a partir da distância (em metros) e do tempo (em segundos).");
+            Console.WriteLine();
+
+            Console.Write(" - Insira a distância percorrida (m): ");
+            distancia = Convert.ToDouble (Console.ReadLine());
+            Console.WriteLine();
+
+
+            Console.Write(" - Insira o tempo gasto (s): ");
+            tempo = Convert.ToDouble (Console.ReadLine());
+            Console.WriteLine();
+
+            velocidade = distancia/tempo;
+
+            Console.WriteLine($" - Velocidade Média: {Math.Floor(velocidade)} m/s.");
+
+
+
+            
+
+
+
+
+
+
+
+
+        }
+    }
+}
